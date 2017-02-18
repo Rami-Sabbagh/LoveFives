3eec0425f7de2e49cf8af966db32295b73070e5f
Rami Sabbagh <ramilego4game@gmail.com>
2017-02-18 19:32:46 +0200

    Nice love2d splash :D

---
+++ states/splash.lua
@@ -0,0 +1,39 @@
+local lovesplashes = require("o-ten-one")
+local tween = require("tween") --linear 230
+
+local splash = lovesplashes( { background = {material.colors.background()}, fill = "lighten" } )
+
+local startfade = false
+local fade = {0}
+local fadeTween = tween.new(1,fade,{255},"inSine")
+
+function love.draw()
+  splash:draw()
+  
+  if startfade then
+    local r, g, b = material.colors.background()
+    love.graphics.setColor(r,g,b,fade[1])
+    love.graphics.rectangle("fill",0,0,_Width,_Height)
+  end
+end
+
+function love.update(dt)
+  splash:update(dt)
+  
+  if startfade then
+    if fadeTween:update(dt) then
+      clearlove()
+      assert(love.filesystem.load("/states/selection.lua"))()
+    end
+  end
+end
+
+function love.keypressed()
+  splash:skip()
+end
+
+love.mousepressed = love.keypressed
+
+function splash.onDone()
+  startfade = true
+end
